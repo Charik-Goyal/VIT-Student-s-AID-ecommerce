9aeadab55d95eba58535da807a9147b2bcb662f8
charik-goyal <charikgoyal8@gmail.com>
2020-12-30 14:37:25 +0530

    initial commit

---
+++ ecommerce-front/src/core/fixedPrices.js
@@ -0,0 +1,32 @@
+export const prices = [
+    {
+        _id: 0,
+        name: "Any",
+        array: []
+    },
+    {
+        _id: 1,
+        name: "₹0 to ₹999",
+        array: [0, 999]
+    },
+    {
+        _id: 2,
+        name: "₹1000 to ₹4999",
+        array: [1000, 5000]
+    },
+    {
+        _id: 3,
+        name: "₹5000 to ₹9999",
+        array: [6000, 9999]
+    },
+    {
+        _id: 4,
+        name: "₹10000 to ₹14999",
+        array: [1000, 14999]
+    },
+    {
+        _id: 5,
+        name: "More than ₹15000",
+        array: [15000, 40000]
+    }
+];
